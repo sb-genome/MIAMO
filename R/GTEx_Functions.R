ac389cd907cbb99a6b6976c2440f434885861261
Dipnaba123 <153063236+Dipnaba123@users.noreply.github.com>
2024-01-18 13:08:40 +0530

    Version_0.0.11

--- R/GTEx_Functions.R
+++ R/GTEx_Functions.R
@@ -1,15 +1,15 @@
-#' Fahrenheit conversion
+#' GTEx Summary Function
 #'
-#' Convert degrees Fahrenheit temperatures to degrees Celsius
+#' A function in R that retrieves a summary of SNP-gene-tissue associations from the Genotype-Tissue Expression GTEx dataset.
 #' @param SNP Single Nucleotide polymorphism or variant
 #' @param Gene Gene Of Interest in the format ENSG00000182179.12
 #' @param Tissue The Tissue of Interest as per GTEx Portal
 #' @return Summary Statistics of the SNP,Gene and Tissue
 #' @examples 
-#' vec <- GTEx_Summary(SNP = "chr3_40_G_T_b38", Gene="ENSG00000182179.12", Tissue="Whole_Blood");
+#' GTEx_Summary(SNP = "chr3_40_G_T_b38", Gene="ENSG00000182179.12", Tissue="Whole_Blood");
 #' @export
 GTEx_Summary <- function(SNP,Gene,Tissue)
 {
-  expr = paste0("k_gtex_fast(V = ",SNP,",G=" , Gene , ",Tissue=",Tissue,")")
-  RSeval()
+  ex = paste0("k_gtex_fast(V='",SNP,"'" , ",G='" , Gene , "'," , "Tissue='",Tissue,"')")
+  return(RSeval(expr = ex , c = connec))
 }
